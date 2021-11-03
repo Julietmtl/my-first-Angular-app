e6dbceb10f80a1d38a9cc9f8cb9c092f9d8e080c
Juliet <li_juliet@hotmail.com>
2021-11-03 11:18:53 -0400

    add first commit, start up project

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  //needs to be a unique selector
+  selector: 'app-server',
+  templateURL: './server.component.html'
+})
+export class ServerComponent {
+
+}
